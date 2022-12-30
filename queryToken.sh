eb6fb1ca88bc36ffa002c7cb5a8b1118b7e6199c
Jeremiah O Sullivan <jeremiah@blockdaemon.com>
2022-12-30 16:19:16 +0000

    Take a snapshot of token owners at 2 intervals and print out the matches.

--- queryToken.sh
+++ queryToken.sh
@@ -1,24 +1,65 @@
+#Cosmwasm smart contract
 CONTRACT=stars10hm2p3ll26zkzwmm202mfdmqy0x0qaxjtqcu6y9cl45razea84hs62p5zn
-TOKEN_ID=1984
 
-QUERY=$(echo '{"all_nft_info":{"token_id":"'$TOKEN_ID'"}}' | base64)
-echo $QUERY
-curl -s "https://rest.elgafar-1.stargaze-apis.com/cosmwasm/wasm/v1/contract/$CONTRACT/smart/$QUERY" | jq -r .data.access.owner
+# Function to check what address has an NFT at a specific time.
+FindTokenOwners() {
+    # create an empty array
+    first_snapshot=()
 
+    # loop 10 times between 1980 and 1990
+    for i in {1983..1984}
+    do
+    # curl the website and store the result in a variable
+    QUERY=$(echo '{"all_nft_info":{"token_id":"'$i'"}}' | base64)
+    result=$(curl -s "https://rest.elgafar-1.stargaze-apis.com/cosmwasm/wasm/v1/contract/$CONTRACT/smart/$QUERY" | jq -r .data.access.owner)
 
-# create an empty array
-results=()
+    # add the result to the array
+    first_snapshot+=("$result")
+    done
 
-# loop 10 times between 1980 and 1990
-for i in {1980..1990}
-do
-  # curl the website and store the result in a variable
-  QUERY=$(echo '{"all_nft_info":{"token_id":"'$i'"}}' | base64)
-  result=$(curl -s "https://rest.elgafar-1.stargaze-apis.com/cosmwasm/wasm/v1/contract/$CONTRACT/smart/$QUERY" | jq -r .data.access.owner)
+    #Print first array.
+    echo "${first_snapshot[@]}"
 
-  # add the result to the array
-  results+=("$result")
-done
+    #Sleep for a random amount of seconds to get 2 snapshot instances then compare.
+    # Generate a random number between 1 and 10
+    random_number=$(expr $RANDOM % 5 + 1)
 
-# print the array
-echo "${results[@]}"
+    # Pause the script for the random number of seconds
+    sleep $random_number
+
+    # Echo the sleeping time
+    echo "Slept for $random_number seconds"
+
+    # create an empty array
+    second_snapshot=()
+
+    # loop 10 times between 1980 and 1990
+    for i in {1983..1984}
+        do
+        # curl the website and store the result in a variable
+        QUERY=$(echo '{"all_nft_info":{"token_id":"'$i'"}}' | base64)
+        result=$(curl -s "https://rest.elgafar-1.stargaze-apis.com/cosmwasm/wasm/v1/contract/$CONTRACT/smart/$QUERY" | jq -r .data.access.owner)
+
+    # add the result to the array
+    second_snapshot+=("$result")
+    done
+
+    # print the second array
+    echo "${second_snapshot[@]}"
+
+    # Compare elements of first snapshot with elements of second snapshot.
+    for element in ${first_snapshot[@]}
+        do
+            for other_element in ${second_snapshot[@]}
+                do
+        if [ "$element" = "$other_element" ] && [ "$element" != "null" ]
+        then
+        echo "$element"
+        fi
+    done
+    done
+
+}
+
+# Call Find Token Owners
+FindTokenOwners
